dff3bc5f5d34da093e5d730da0a3284a1baf81a2
Thomas antony <77287334+lordwiz@users.noreply.github.com>
2023-03-31 02:27:34 -0700

    Add files via upload

--- CO1-2/matrices.java
+++ CO1-2/matrices.java
@@ -1,1 +1,31 @@
-Read 2 matrices from the console and perform matrix addition.
+import java.util.Scanner;
+public class matrices{  
+public static void main(String args[]){  
+   
+ int m, n, i, j;
+        Scanner in = null;
+            in = new Scanner(System.in);
+            System.out.println("Enter the number "+ "of rows of the matrix");
+            m = in.nextInt();
+            System.out.println("Enter the number "+ "of columns of the matrix");
+            n = in.nextInt();
+   
+            int first[][] = new int[m][n];
+  
+
+            System.out.println("Enter the elements of the matrix");
+            for (i = 0; i < m; i++)
+                for (j = 0; j < n; j++)
+                    first[i][j] = in.nextInt();
+ 
+            System.out.println("Elements of the matrix are");
+            for (i = 0; i < m; i++) {
+                for (j = 0; j < n; j++)
+                    System.out.print(first[i][j] + "  ");
+                System.out.println();
+in.close();   
+}   
+ 
+System.out.println();   
+}    
+} 
